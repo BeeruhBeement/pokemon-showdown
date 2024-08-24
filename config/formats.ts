8e8128df47e3330e22fe51da8089eb0ca74d3e19
BeeruhBeement <58148912+BeeruhBeement@users.noreply.github.com>
2024-08-24 12:53:06 +0200

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -130,51 +130,36 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		name: "[Gen 3] Modded UU",
 
 		mod: 'gen3mod',
-		ruleset: ['Standard', 'Data Mod'],
-		banlist: [
-			'Uber', 'OU', 'UUBL', 
-			'Heat Rock', 'Damp Rock'
-		],
+		ruleset: ['[Gen 3] Modded'],
+		banlist: ['OU', 'UUBL', 'Heat Rock', 'Damp Rock'],
 	},
 	{
 		name: "[Gen 3] Modded RU",
 
 		mod: 'gen3mod',
-		ruleset: ['Standard', 'Data Mod'],
-		banlist: [
-			'Uber', 'OU', 'UUBL', 'UU', 'RUBL', 
-			'Heat Rock', 'Damp Rock'
-		],
+		ruleset: ['[Gen 3] Modded UU'],
+		banlist: ['UU', 'RUBL'],
 	},
 	{
 		name: "[Gen 3] Modded NU",
 
 		mod: 'gen3mod',
-		ruleset: ['Standard', 'Data Mod'],
-		banlist: [
-			'Uber', 'OU', 'UUBL', 'UU', 'RUBL', 'RU', 'NUBL', 
-			'Heat Rock', 'Damp Rock'
-		],
+		ruleset: ['[Gen 3] Modded RU'],
+		banlist: ['RU', 'NUBL'],
 	},
 	{
 		name: "[Gen 3] Modded PU",
 
 		mod: 'gen3mod',
-		ruleset: ['Standard', 'Data Mod'],
-		banlist: [
-			'Uber', 'OU', 'UUBL', 'UU', 'RUBL', 'RU', 'NUBL', 'NU', 'PUBL', 
-			'Heat Rock', 'Damp Rock'
-		],
+		ruleset: ['[Gen 3] Modded NU'],
+		banlist: ['NU', 'PUBL'],
 	},
 	{
 		name: "[Gen 3] Modded ZU",
 
 		mod: 'gen3mod',
-		ruleset: ['Standard', 'Data Mod'],
-		banlist: [
-			'Uber', 'OU', 'UUBL', 'UU', 'RUBL', 'RU', 'NUBL', 'NU', 'PUBL', 'PU', 'ZUBL', 
-			'Heat Rock', 'Damp Rock'
-		],
+		ruleset: ['[Gen 3] Modded PU'],
+		banlist: ['PU', 'ZUBL'],
 	},
 	{
 		name: "[Gen 3] Modded NFE",
