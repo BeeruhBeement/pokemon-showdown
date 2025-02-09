cd14dfa13c928aea244c6d05a57428a2dc31cad8
BeeruhBeement <58148912+BeeruhBeement@users.noreply.github.com>
2025-02-09 18:51:17 +0100

    no hp fairy/sound

--- sim/dex.ts
+++ sim/dex.ts
@@ -321,7 +321,6 @@ export class ModdedDex {
 		const hpTypes = [
 			'Fighting', 'Flying', 'Poison', 'Ground', 'Rock', 'Bug', 'Ghost', 'Steel',
 			'Fire', 'Water', 'Grass', 'Electric', 'Psychic', 'Ice', 'Dragon', 'Dark',
-			'Fairy', 'Sound',
 		];
 		const tr = this.trunc;
 		const stats = {hp: 31, atk: 31, def: 31, spe: 31, spa: 31, spd: 31};
