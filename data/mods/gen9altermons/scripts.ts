9e14c59e4e6c26844e6c6136e035195b7ccda2ac
BeeruhBeement <58148912+BeeruhBeement@users.noreply.github.com>
2025-09-21 15:28:09 +0200

    this doesn't actually work

--- data/mods/gen9altermons/scripts.ts
+++ data/mods/gen9altermons/scripts.ts
@@ -1,12 +1,4 @@
 export const Scripts: ModdedBattleScriptsData = {
 	gen: 9,
 	inherit: 'gen9',
-	init() {
-		for (const i in this.data.Abilities) {
-			this.modData('Abilities', i).isNonstandard = null;
-		}
-		for (const i in this.data.Moves) {
-			this.modData('Moves', i).isNonstandard = null;
-		}
-	},
 };
