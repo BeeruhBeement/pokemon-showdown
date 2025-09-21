95ee1ec884f825545bb37639ffee7f19e8351216
BeeruhBeement <58148912+BeeruhBeement@users.noreply.github.com>
2025-09-21 13:43:02 +0200

    there's just no way

--- data/mods/fakemonhq/pokedex.ts
+++ data/mods/fakemonhq/pokedex.ts
@@ -1,7 +1,7 @@
 export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
 	citricity: {
 		num: -90000,
-		name: "Citricty",
+		name: "Citricity",
 		types: ["Poison", "Electric"],
 		gender: "N",
 		baseStats: { hp: 53, atk: 149, def: 97, spa: 113, spd: 79, spe: 79 },
