44fc6bd0f5be8b9f90c16add89e3def5ab52e71c
BeeruhBeement <58148912+BeeruhBeement@users.noreply.github.com>
2025-09-27 20:47:00 +0200

    slate 0 altermons

--- data/mods/gen9altermons/pokedex.ts
+++ data/mods/gen9altermons/pokedex.ts
@@ -3,85 +3,9 @@ export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable 
 		inherit: true,
 		types: ["Normal"],
 	},
-	beheeyem: {
-		inherit: true,
-		types: ["Fire", "Dragon"],
-	},
-	lucario: {
-		inherit: true,
-		types: ["Bug", "Dark"],
-	},
-	lucariomega: {
-		inherit: true,
-		types: ["Bug", "Dark"],
-	},
-	crawdaunt: {
-		inherit: true,
-		types: ["Ground", "Fire"],
-	},
-	druddigon: {
-		inherit: true,
-		types: ["Steel"],
-	},
-	hydrapple: {
-		inherit: true,
-		types: ["Psychic", "Fighting"],
-	},
-	swampert: {
-		inherit: true,
-		types: ["Dark", "Poison"],
-	},
-	spinda: {
-		inherit: true,
-		types: ["Fire"],
-	},
-	latias: {
-		inherit: true,
-		types: ["Electric", "Steel"],
-	},
-	electrode: {
-		inherit: true,
-		types: ["Fighting", "Flying"],
-	},
-	flapple: {
-		inherit: true,
-		types: ["Psychic", "Electric"],
-	},
-	appletun: {
-		inherit: true,
-		types: ["Psychic", "Fairy"],
-	},
-	banette: {
-		inherit: true,
-		types: ["Fighting"],
-	},
-	banettemega: {
-		inherit: true,
-		types: ["Fighting", "Fairy"],
-	},
-	skeledirge: {
-		inherit: true,
-		types: ["Steel", "Fairy"],
-	},
-	kyogre: {
-		inherit: true,
-		types: ["Fire"],
-	},
-	rhyperior: {
-		inherit: true,
-		types: ["Water", "Poison"],
-	},
 	centiskorch: {
 		inherit: true,
 		types: ["Steel", "Grass"],
-	},
-	bronzong: {
-		inherit: true,
-		types: ["Rock", "Poison"],
-	},
-	mantine: {
-		inherit: true,
-		types: ["Dark", "Ground"],
 	},
 	salazzle: {
 		inherit: true,
@@ -90,6 +14,14 @@ export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable 
 	klawf: {
 		inherit: true,
 		types: ["Water"],
+	},
+	mantine: {
+		inherit: true,
+		types: ["Dark", "Ground"],
+	},
+	skeledirge: {
+		inherit: true,
+		types: ["Steel", "Fairy"],
 	},
 	accelgor: {
 		inherit: true,
@@ -114,5 +46,149 @@ export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable 
 	espathra: {
 		inherit: true,
 		types: ["Ice", "Rock"],
+	},
+	kyogre: {
+		inherit: true,
+		types: ["Fire"],
+	},
+	reuniclus: {
+		inherit: true,
+		types: ["Grass", "Poison"],
+	},
+	scyther: {
+		inherit: true,
+		types: ["Dragon", "Grass"],
+	},
+	scizor: {
+		inherit: true,
+		types: ["Electric", "Ice"],
+	},
+	kleavor: {
+		inherit: true,
+		types: ["Dark", "Poison"],
+	},
+	cryogonal: {
+		inherit: true,
+		types: ["Steel", "Psychic"],
+	},
+	cofagrigus: {
+		inherit: true,
+		types: ["Flying"],
+	},
+	rhyperior: {
+		inherit: true,
+		types: ["Water", "Poison"],
+	},
+	grafaiai: {
+		inherit: true,
+		types: ["Dark", "Fire"],
+	},
+	hatterene: {
+		inherit: true,
+		types: ["Water"],
+	},
+	banette: {
+		inherit: true,
+		types: ["Fighting"],
+	},
+	banettemega: {
+		inherit: true,
+		types: ["Fighting", "Fairy"],
+	},
+	flygon: {
+		inherit: true,
+		types: ["Water", "Steel"],
+	},
+	heliolisk: {
+		inherit: true,
+		types: ["Dark", "Dragon"],
+	},
+	ambipom: {
+		inherit: true,
+		types: ["Ground", "Flying"],
+	},
+	cosmoem: {
+		inherit: true,
+		types: ["Steel", "Flying"],
+	},
+	lunala: {
+		inherit: true,
+		types: ["Fairy", "Dark"],
+	},
+	solgaleo: {
+		inherit: true,
+		types: ["Normal", "Fire"],
+	},
+	beheeyem: {
+		inherit: true,
+		types: ["Fire", "Dragon"],
+	},
+	lucario: {
+		inherit: true,
+		types: ["Bug", "Dark"],
+	},
+	lucariomega: {
+		inherit: true,
+		types: ["Bug", "Dark"],
+	},
+	crawdaunt: {
+		inherit: true,
+		types: ["Ground", "Fire"],
+	},
+	druddigon: {
+		inherit: true,
+		types: ["Steel"],
+	},
+	hydrapple: {
+		inherit: true,
+		types: ["Psychic", "Fighting"],
+	},
+	galvantula: {
+		inherit: true,
+		types: ["Normal", "Ice"],
+	},
+	swampert: {
+		inherit: true,
+		types: ["Dark", "Poison"],
+	},
+	altaria: {
+		inherit: true,
+		types: ["Grass", "Fire"],
+	},
+	altariamega: {
+		inherit: true,
+		types: ["Grass", "Fire"],
+	},
+	spinda: {
+		inherit: true,
+		types: ["Fire"],
+	},
+	coalossal: {
+		inherit: true,
+		types: ["Water", "Grass"],
+	},
+	latias: {
+		inherit: true,
+		types: ["Electric", "Steel"],
+	},
+	electrode: {
+		inherit: true,
+		types: ["Fighting", "Flying"],
+	},
+	flapple: {
+		inherit: true,
+		types: ["Psychic", "Electric"],
+	},
+	appletun: {
+		inherit: true,
+		types: ["Psychic", "Fairy"],
+	},
+	dhelmise: {
+		inherit: true,
+		types: ["Fighting", "Flying"],
+	},
+	bronzong: {
+		inherit: true,
+		types: ["Rock", "Poison"],
 	},
 };
