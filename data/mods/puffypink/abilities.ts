10e0b33685c7e4595859d641ccc1f40b4fd9f7c1
BeeruhBeement <58148912+BeeruhBeement@users.noreply.github.com>
2025-11-01 14:32:45 +0100

    finished kirby pokedex

--- data/mods/puffypink/abilities.ts
+++ data/mods/puffypink/abilities.ts
@@ -10,4 +10,23 @@ export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTa
 		desc: "Pokemon making contact with this Pokemon lose 1/16 of their maximum HP, rounded down. This effect does not happen if this Pokemon did not lose HP from the attack.",
 		shortDesc: "Pokemon making contact with this Pokemon lose 1/16 of their max HP.",
 	},
+	pitchblack: {
+		onModifyTypePriority: -1,
+		onModifyType(move, pokemon) {
+			const noModifyType = [
+				'judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'terrainpulse', 'weatherball',
+			];
+			if (move.type === 'Normal' && (!noModifyType.includes(move.id) || this.activeMove?.isMax) &&
+				!(move.isZ && move.category !== 'Status') && !(move.name === 'Tera Blast' && pokemon.terastallized)) {
+				move.type = 'Dark';
+				move.typeChangerBoosted = this.effect;
+			}
+		},
+		flags: {},
+		name: "Pitch Black",
+		rating: 4,
+		num: 0,
+		desc: "This Pokemon's Normal-type moves become Dark-type moves. This effect comes after other effects that change a move's type, but before Ion Deluge and Electrify's effects.",
+		shortDesc: "This Pokemon's Normal-type moves become Dark type.",
+	},
 }
---
+++ data/mods/puffypink/formats-data.ts
@@ -0,0 +1,602 @@
+export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormatsDataTable = {
+	whispyjr: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	whispywoods: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	yggywoods: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	bobo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	flamer: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	mrbright: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	gussa: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	joe: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	gobbler: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	pupa: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	como: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	spideroo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	capiller: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	needlous: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	pierce: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	brontoburt: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	birdon: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	dynablade: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	awoofy: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	garuroofy: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	blockin: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	blocky: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	cappy: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	poisonmash: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	krackojr: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	kracko: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	kabu: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	dekabu: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	waddledee: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	bigdee: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	granddee: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	puppetdee: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	tankdee: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	bandanadee: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	caper: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	tedhaun: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	embird: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	hotwings: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	puffy: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	coolspook: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	blinkbat: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	orbserver: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	flora: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	lovely: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	ladyivy: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	beanbon: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	zombon: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	beetley: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	hornhead: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	gawgaw: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	gaogao: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	boxin: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	boxboxer: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	waddledoo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	kingdoo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	mookie: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	bonkers: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	pompey: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	pyribbit: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	blipper: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	mamatee: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	fattywhale: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	dupa: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	dippa: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	deppa: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	sandran: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	ironmam: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	grandmam: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	mrpumpkin: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	squashini: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	chemitory: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	squishy: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	mastergreen: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	gansan: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	rocky: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	moundo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	galbo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	flamegalboros: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	lobzilla: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	ebizou: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	ufo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	dubior: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	corori: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	drifter: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	jumpershoot: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	frigis: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	snowl: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	tortorner: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	tortuilding: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	slippy: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	poisoncroakom: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	skuller: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	skully: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	skullseer: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	madoo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	wiz: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	snoppy: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	mrfrosty: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	armoroid: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	brobo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	boten: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	totenga: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	phanta: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	acephanta: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	masterhand: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	crazyhand: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	scarfy: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	venog: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	miasmoros: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	gordo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	blado: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	plasmawisp: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	simirror: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	buffahorn: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	glunk: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	deepsee: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	gator: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	gnawcodile: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	flotzo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	octacon: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	nesp: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	caller: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	chilly: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	plugg: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	burninleo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	bugzzy: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	grizzo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	butterfly: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	morphoknight: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	heavylobster: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	droppy: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	darkmatter: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	miraclematter: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	darkmatterblade: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	gooey: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	computervirus: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	qbby: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	qudy: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	jobski: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	jobsking: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	metroid: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	noddy: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	kine: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	rick: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	coo: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	icedragon: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	tornadon: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	landia: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	void: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	kirby: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+}
