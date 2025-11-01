6e699dde877157f9bb16a073a3f3b1383d09a49b
BeeruhBeement <58148912+BeeruhBeement@users.noreply.github.com>
2025-11-01 23:53:28 +0100

    forgot mega mie sprites

--- config/formats.ts
+++ config/formats.ts
@@ -48,7 +48,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 
 		mod: 'gen3puffypink',
 		ruleset: ['Standard', 'Data Mod'],
-		banlist: ['Uber', 'Drizzle + Swift Swim', 'Sand Stream + Sand Veil'],
+		banlist: ['Uber', 'Drizzle ++ Swift Swim', 'Sand Stream ++ Sand Veil'],
 	},
 
 	{
