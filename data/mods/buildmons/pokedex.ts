6c7af737618fc577978e38480a99b73752381991
BeeruhBeement <58148912+BeeruhBeement@users.noreply.github.com>
2026-03-29 13:16:32 +0200

    ballfetch over noabil

--- data/mods/buildmons/pokedex.ts
+++ data/mods/buildmons/pokedex.ts
@@ -3,19 +3,19 @@ import { ModdedSpeciesData } from "../../../sim/dex-species";
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	poliwrath: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	rapidashgalar: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	seaking: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	granbull: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	donphan: {
 		inherit: true,
@@ -23,19 +23,19 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	},
 	shiftry: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	masquerain: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	exploud: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	hariyama: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	sableye: {
 		inherit: true,
@@ -43,91 +43,91 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	},
 	manectric: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	camerupt: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	claydol: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	rampardos: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	bastiodon: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	drifblim: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	abomasnow: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	simisage: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	simisear: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	simipour: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	seismitoad: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	vanilluxe: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	slurpuff: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	toucannon: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	shiinotic: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	bewear: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	turtonator: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	togedemaru: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	dubwool: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	perrserker: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	scovillain: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	wugtrio: {
 		inherit: true,
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 	bombirdier: {
 		inherit: true,
@@ -136,6 +136,6 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	houndstone: {
 		inherit: true,
 		types: ["Ghost", "Rock"],
-		abilities: { 0: "No Ability" }
+		abilities: { 0: "Ball Fetch" }
 	},
 };
