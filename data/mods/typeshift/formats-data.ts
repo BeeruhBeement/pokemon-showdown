18852e639cfb68bc8a9d9a8d42d352ba40a91cb8
BeeruhBeement <58148912+BeeruhBeement@users.noreply.github.com>
2026-04-14 22:55:44 +0200

    typeshift tiershift

---
+++ data/mods/typeshift/formats-data.ts
@@ -0,0 +1,35 @@
+export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormatsDataTable = {
+	deoxysspeed: {
+		natDexTier: "OU",
+	},
+	gougingfire: {
+		natDexTier: "OU",
+	},
+	darkrai: {
+		natDexTier: "OU",
+	},
+	dragapult: {
+		natDexTier: "OU",
+	},
+	palafin: {
+		natDexTier: "OU",
+	},
+	magearna: {
+		natDexTier: "OU",
+	},
+	ursalunabloodmoon: {
+		natDexTier: "OU",
+	},
+	walkingwake: {
+		natDexTier: "OU",
+	},
+	ironbundle: {
+		natDexTier: "OU",
+	},
+	ogerponheartflame: {
+		natDexTier: "OU",
+	},
+	shedinja: {
+		natDexTier: "Uber",
+	},
+};
