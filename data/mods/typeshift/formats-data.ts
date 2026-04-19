06c0c525d2809f71942f0b71c0b638fc03148f63
BeeruhBeement <58148912+BeeruhBeement@users.noreply.github.com>
2026-04-19 14:41:34 +0200

    type shift tier shifts

--- data/mods/typeshift/formats-data.ts
+++ data/mods/typeshift/formats-data.ts
@@ -30,4 +30,74 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 	zamazenta: {
 		natDexTier: "Uber",
 	},
+	
+	clefablemega: {
+		tier: "OU",
+	},
+	victreebelmega: {
+		tier: "OU",
+	},
+	starmiemega: {
+		tier: "OU",
+	},
+	dragonitemega: {
+		tier: "OU",
+	},
+	meganiummega: {
+		tier: "OU",
+	},
+	feraligatrmega: {
+		tier: "OU",
+	},
+	skarmorymega: {
+		tier: "OU",
+	},
+	chimechomega: {
+		tier: "OU",
+	},
+	froslassmega: {
+		tier: "OU",
+	},
+	emboarmega: {
+		tier: "OU",
+	},
+	excadrillmega: {
+		tier: "OU",
+	},
+	chandeluremega: {
+		tier: "OU",
+	},
+	golurkmega: {
+		tier: "OU",
+	},
+	chesnaughtmega: {
+		tier: "OU",
+	},
+	delphoxmega: {
+		tier: "OU",
+	},
+	greninjamega: {
+		tier: "OU",
+	},
+	floettemega: {
+		tier: "OU",
+	},
+	meowsticmega: {
+		tier: "OU",
+	},
+	hawluchamega: {
+		tier: "OU",
+	},
+	crabominablemega: {
+		tier: "OU",
+	},
+	drampamega: {
+		tier: "OU",
+	},
+	scovillainmega: {
+		tier: "OU",
+	},
+	glimmoramega: {
+		tier: "OU",
+	},
 };
---
+++ data/mods/typeshift/items.ts
@@ -0,0 +1,94 @@
+export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
+	chandelurite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	chesnaughtite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	chimechite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	clefablite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	crabominite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	delphoxite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	dragoninite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	drampanite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	emboarite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	excadrite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	feraligite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	floettite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	froslassite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	glimmoranite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	golurkite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	greninjite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	hawluchanite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	meganiumite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	meowsticite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	scovillainite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	skarmorite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	starminite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	victreebelite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+};
