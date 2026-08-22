417e7739ee8974a61982ab791ce5239dc860ba53
Beeruh <diuvermartinez1@gmail.com>
2026-08-22 17:36:13 +0200

    altermons nonstandard modded

--- data/mods/gen9altermons/pokedex.ts
+++ data/mods/gen9altermons/pokedex.ts
@@ -2,389 +2,486 @@ export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable 
 	luxray: {
 		inherit: true,
 		types: ["Normal"],
+		isNonstandard: "Modded",
 	},
 	centiskorch: {
 		inherit: true,
 		types: ["Steel", "Grass"],
+		isNonstandard: "Modded",
 	},
 	salazzle: {
 		inherit: true,
 		types: ["Bug", "Electric"],
+		isNonstandard: "Modded",
 	},
 	klawf: {
 		inherit: true,
 		types: ["Water"],
+		isNonstandard: "Modded",
 	},
 	mantine: {
 		inherit: true,
 		types: ["Dark", "Ground"],
+		isNonstandard: "Modded",
 	},
 	skeledirge: {
 		inherit: true,
 		types: ["Steel", "Fairy"],
+		isNonstandard: "Modded",
 	},
 	accelgor: {
 		inherit: true,
 		types: ["Ghost"],
+		isNonstandard: "Modded",
 	},
 	hydreigon: {
 		inherit: true,
 		types: ["Psychic", "Poison"],
+		isNonstandard: "Modded",
 	},
 	gyarados: {
 		inherit: true,
 		types: ["Ground", "Dragon"],
+		isNonstandard: "Modded",
 	},
 	gyaradosmega: {
 		inherit: true,
 		types: ["Ground", "Dragon"],
+		isNonstandard: "Modded",
 	},
 	tsareena: {
 		inherit: true,
 		types: ["Fighting", "Rock"],
+		isNonstandard: "Modded",
 	},
 	bewear: {
 		inherit: true,
 		types: ["Ghost", "Fairy"],
+		isNonstandard: "Modded",
 	},
 	espathra: {
 		inherit: true,
 		types: ["Ice", "Rock"],
+		isNonstandard: "Modded",
 	},
 	kyogre: {
 		inherit: true,
 		types: ["Fire"],
+		isNonstandard: "Modded",
 	},
 	kyogreprimal: {
 		inherit: true,
 		types: ["Fire"],
+		isNonstandard: "Modded",
 	},
 	reuniclus: {
 		inherit: true,
 		types: ["Grass", "Poison"],
+		isNonstandard: "Modded",
 	},
 	scyther: {
 		inherit: true,
 		types: ["Dragon", "Grass"],
+		isNonstandard: "Modded",
 	},
 	scizor: {
 		inherit: true,
 		types: ["Electric", "Ice"],
+		isNonstandard: "Modded",
 	},
 	scizormega: {
 		inherit: true,
 		types: ["Electric", "Ice"],
+		isNonstandard: "Modded",
 	},
 	kleavor: {
 		inherit: true,
 		types: ["Dark", "Poison"],
+		isNonstandard: "Modded",
 	},
 	cryogonal: {
 		inherit: true,
 		types: ["Steel", "Psychic"],
+		isNonstandard: "Modded",
 	},
 	cofagrigus: {
 		inherit: true,
 		types: ["Flying"],
+		isNonstandard: "Modded",
 	},
 	rhyperior: {
 		inherit: true,
 		types: ["Water", "Poison"],
+		isNonstandard: "Modded",
 	},
 	grafaiai: {
 		inherit: true,
 		types: ["Dark", "Fire"],
+		isNonstandard: "Modded",
 	},
 	hatterene: {
 		inherit: true,
 		types: ["Water"],
+		isNonstandard: "Modded",
 	},
 	banette: {
 		inherit: true,
 		types: ["Fighting"],
+		isNonstandard: "Modded",
 	},
 	banettemega: {
 		inherit: true,
 		types: ["Fighting", "Fairy"],
+		isNonstandard: "Modded",
 	},
 	flygon: {
 		inherit: true,
 		types: ["Water", "Steel"],
+		isNonstandard: "Modded",
 	},
 	heliolisk: {
 		inherit: true,
 		types: ["Dark", "Dragon"],
+		isNonstandard: "Modded",
 	},
 	ambipom: {
 		inherit: true,
 		types: ["Ground", "Flying"],
+		isNonstandard: "Modded",
 	},
 	cosmoem: {
 		inherit: true,
 		types: ["Steel", "Flying"],
+		isNonstandard: "Modded",
 	},
 	lunala: {
 		inherit: true,
 		types: ["Fairy", "Dark"],
+		isNonstandard: "Modded",
 	},
 	solgaleo: {
 		inherit: true,
 		types: ["Normal", "Fire"],
+		isNonstandard: "Modded",
 	},
 	beheeyem: {
 		inherit: true,
 		types: ["Fire", "Dragon"],
+		isNonstandard: "Modded",
 	},
 	lucario: {
 		inherit: true,
 		types: ["Bug", "Dark"],
+		isNonstandard: "Modded",
 	},
 	lucariomega: {
 		inherit: true,
 		types: ["Bug", "Dark"],
+		isNonstandard: "Modded",
 	},
 	crawdaunt: {
 		inherit: true,
 		types: ["Ground", "Fire"],
+		isNonstandard: "Modded",
 	},
 	druddigon: {
 		inherit: true,
 		types: ["Steel"],
+		isNonstandard: "Modded",
 	},
 	hydrapple: {
 		inherit: true,
 		types: ["Psychic", "Fighting"],
+		isNonstandard: "Modded",
 	},
 	galvantula: {
 		inherit: true,
 		types: ["Normal", "Ice"],
+		isNonstandard: "Modded",
 	},
 	swampert: {
 		inherit: true,
 		types: ["Dark", "Poison"],
+		isNonstandard: "Modded",
 	},
 	swampertmega: {
 		inherit: true,
 		types: ["Dark", "Poison"],
+		isNonstandard: "Modded",
 	},
 	altaria: {
 		inherit: true,
 		types: ["Grass", "Fire"],
+		isNonstandard: "Modded",
 	},
 	altariamega: {
 		inherit: true,
 		types: ["Grass", "Fire"],
+		isNonstandard: "Modded",
 	},
 	spinda: {
 		inherit: true,
 		types: ["Fire"],
+		isNonstandard: "Modded",
 	},
 	coalossal: {
 		inherit: true,
 		types: ["Water", "Grass"],
+		isNonstandard: "Modded",
 	},
 	latias: {
 		inherit: true,
 		types: ["Electric", "Steel"],
+		isNonstandard: "Modded",
 	},
 	latiasmega: {
 		inherit: true,
 		types: ["Electric", "Steel"],
+		isNonstandard: "Modded",
 	},
 	electrode: {
 		inherit: true,
 		types: ["Fighting", "Flying"],
+		isNonstandard: "Modded",
 	},
 	flapple: {
 		inherit: true,
 		types: ["Psychic", "Electric"],
+		isNonstandard: "Modded",
 	},
 	appletun: {
 		inherit: true,
 		types: ["Psychic", "Fairy"],
+		isNonstandard: "Modded",
 	},
 	dhelmise: {
 		inherit: true,
 		types: ["Fighting", "Flying"],
+		isNonstandard: "Modded",
 	},
 	bronzong: {
 		inherit: true,
 		types: ["Rock", "Poison"],
+		isNonstandard: "Modded",
 	},
 	zapdosgalar: {
 		inherit: true,
 		types: ["Dark", "Dragon"],
+		isNonstandard: "Modded",
 	},
 	articunogalar: {
 		inherit: true,
 		types: ["Electric", "Dragon"],
+		isNonstandard: "Modded",
 	},
 	moltresgalar: {
 		inherit: true,
 		types: ["Fighting", "Dragon"],
+		isNonstandard: "Modded",
 	},
 	houndoom: {
 		inherit: true,
 		types: ["Ice", "Ghost"],
+		isNonstandard: "Modded",
 	},
 	houndoommega: {
 		inherit: true,
 		types: ["Ice", "Ghost"],
+		isNonstandard: "Modded",
 	},
 	clefable: {
 		inherit: true,
 		types: ["Fire", "Steel"],
+		isNonstandard: "Modded",
 	},
 	heatran: {
 		inherit: true,
 		types: ["Rock", "Fairy"],
+		isNonstandard: "Modded",
 	},
 	aegislash: {
 		inherit: true,
 		types: ["Ice", "Fighting"],
+		isNonstandard: "Modded",
 	},
 	aegislashblade: {
 		inherit: true,
 		types: ["Ice", "Fighting"],
+		isNonstandard: "Modded",
 	},
 	glimmora: {
 		inherit: true,
 		types: ["Grass", "Ground"],
+		isNonstandard: "Modded",
 	},
 	gliscor: {
 		inherit: true,
 		types: ["Normal", "Ghost"],
+		isNonstandard: "Modded",
 	},
 	latios: {
 		inherit: true,
 		types: ["Electric", "Fire"],
+		isNonstandard: "Modded",
 	},
 	latiosmega: {
 		inherit: true,
 		types: ["Electric", "Fire"],
+		isNonstandard: "Modded",
 	},
 	tapukoko: {
 		inherit: true,
 		types: ["Bug", "Water"],
+		isNonstandard: "Modded",
 	},
 	tapulele: {
 		inherit: true,
 		types: ["Rock", "Ground"],
+		isNonstandard: "Modded",
 	},
 	tapubulu: {
 		inherit: true,
 		types: ["Ghost", "Steel"],
+		isNonstandard: "Modded",
 	},
 	tapufini: {
 		inherit: true,
 		types: ["Dragon", "Poison"],
+		isNonstandard: "Modded",
 	},
 	dudunsparce: {
 		inherit: true,
 		types: ["Poison", "Flying"],
+		isNonstandard: "Modded",
 	},
 	poliwrath: {
 		inherit: true,
 		types: ["Electric", "Dark"],
+		isNonstandard: "Modded",
 	},
 	politoed: {
 		inherit: true,
 		types: ["Electric", "Flying"],
+		isNonstandard: "Modded",
 	},
 	eelektross: {
 		inherit: true,
 		types: ["Ghost", "Fighting"],
+		isNonstandard: "Modded",
 	},
 	sableye: {
 		inherit: true,
 		types: ["Ground", "Fairy"],
+		isNonstandard: "Modded",
 	},
 	sableyemega: {
 		inherit: true,
 		types: ["Ground", "Fairy"],
+		isNonstandard: "Modded",
 	},
 	guzzlord: {
 		inherit: true,
 		types: ["Ground", "Steel"],
+		isNonstandard: "Modded",
 	},
 	sigilyph: {
 		inherit: true,
 		types: ["Water", "Fairy"],
+		isNonstandard: "Modded",
 	},
 	farigiraf: {
 		inherit: true,
 		types: ["Fairy", "Dragon"],
+		isNonstandard: "Modded",
 	},
 	palafin: {
 		inherit: true,
 		types: ["Normal", "Fighting"],
+		isNonstandard: "Modded",
 	},
 	palafinhero: {
 		inherit: true,
 		types: ["Bug", "Fighting"],
+		isNonstandard: "Modded",
 	},
 	miltank: {
 		inherit: true,
 		types: ["Fighting", "Ground"],
+		isNonstandard: "Modded",
 	},
 	alomomola: {
 		inherit: true,
 		types: ["Fire", "Flying"],
+		isNonstandard: "Modded",
 	},
 	greattusk: {
 		inherit: true,
 		types: ["Dark", "Steel"],
+		isNonstandard: "Modded",
 	},
 	clodsire: {
 		inherit: true,
 		types: ["???"],
+		isNonstandard: "Modded",
 	},
 	floetteeternal: {
 		inherit: true,
 		types: ["Grass", "Electric"],
+		isNonstandard: "Modded",
 	},
 	floettemega: {
 		inherit: true,
 		types: ["Grass", "Electric"],
+		isNonstandard: "Modded",
 	},
 	gigalith: {
 		inherit: true,
 		types: ["Ice", "Ground"],
+		isNonstandard: "Modded",
 	},
 	uxie: {
 		inherit: true,
 		types: ["Dark", "Fairy"],
+		isNonstandard: "Modded",
 	},
 	azelf: {
 		inherit: true,
 		types: ["Poison", "Fighting"],
+		isNonstandard: "Modded",
 	},
 	mesprit: {
 		inherit: true,
 		types: ["Dragon", "Steel"],
+		isNonstandard: "Modded",
 	},
 	bellibolt: {
 		inherit: true,
 		types: ["Fairy", "Normal"],
+		isNonstandard: "Modded",
 	},
 	kyurem: {
 		inherit: true,
 		types: ["Rock", "Flying"],
+		isNonstandard: "Modded",
 	},
 	blissey: {
 		inherit: true,
 		types: ["Dark"],
+		isNonstandard: "Modded",
 	},
 	flareon: {
 		inherit: true,
 		types: ["Steel", "Electric"],
+		isNonstandard: "Modded",
 	},
 	drifblim: {
 		inherit: true,
 		types: ["Electric", "Fairy"],
+		isNonstandard: "Modded",
 	},
 	celebi: {
 		inherit: true,
 		types: ["Dark", "Flying"],
+		isNonstandard: "Modded",
 	},
 };
